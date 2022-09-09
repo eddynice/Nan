1e950fdcf04c941ffd501c4ff7d737bfa9a38d52
Osaze <osareniyeosazee@gmail.com>
2022-09-10 00:42:21 +0100

    added

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -1,8 +1,9 @@
 
     import React from 'react';
 import { useForm } from 'react-hook-form';
+import classes from "./contact.module.css"
 
-export default function App() {
+export default function Contact() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
   console.log(errors);
@@ -11,17 +12,19 @@ export default function App() {
     <>
    
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Name" {...register("Name", {})} />
-      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-      <input {...register("firstName", { required: true, maxLength: 20 })} />
-      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-      <input type="number" {...register("age", { min: 18, max: 99 })} />
-      <input type="submit" />
-      <input {...register("Developer", { required: true })} type="radio" value="Yes" />
-      <input {...register("Developer", { required: true })} type="radio" value="No" />
+      <div className={classes.text}>
+      <input type="text" placeholder="Name" className="" {...register("Name", {})} />
+      </div>
+      <div className={classes.email}>
+      <input type="email" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+      </div>
+      <div className={classes.subject}>
+     
       <input type="text" placeholder="Subject" {...register("Subject", {required: true})} />
-      <input type="text" placeholder="Message" {...register("Message", {})} />
-
+      </div>
+      <div className={classes.message}>
+      <textarea type="textarea" placeholder="Message" {...register("Message", {})} />
+      </div>
       <input type="submit" />
     </form>
     
