5b3d0cf91cf26360d23a81f14a4e5598717417a5
Osaze <osareniyeosazee@gmail.com>
2022-12-21 00:00:40 +0100

    sumbitting function working

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -1,17 +1,28 @@
+import React,{useState} from 'react';
+import  "./contact.css";
+import emailjs from 'emailjs-com';
 
-    import React,{useState} from 'react';
-//import { useForm } from 'react-hook-form';
-import  "./contact.css"
 
 export default function Contact() {
-  //const { register, handleSubmit, formState: { errors } } = useForm();
-  
-  const [toSend, setTosend] = useState({name:"",
-email:"",number:"",message:"",
+  const [toSend, setTosend] = useState({from_name:"",
+  email:"",number:"",message:"",
 })
+
 const onSubmit =(e) => {
   e.preventDefault()
+  setTosend({
+    from_name:"",
+    email:"",number:"",message:"",
+  })
+  emailjs.send("service_0teafvs","template_z08gfgv",toSend,"arzK141-u7gAHVTUG")
+  .then((response)=>{
+    console.log("Sucess", response.status, response.text);
+  })
+  .catch((err)=>{
+    console.log("Failed", err)
+  })
 }
+
  const handleChange = (e)=>{
   setTosend({ ...toSend, [e.target.name]: e.target.value})
  }
@@ -36,7 +47,7 @@ const onSubmit =(e) => {
   <div className="right">
     <form onSubmit={onSubmit}   className="form">
       <div className="formContrl">
-        <input type="text" name="name" value={toSend.name} onChange={handleChange} placeholder='fullName'/> 
+        <input type="text" name="from_name" value={toSend.from_name} onChange={handleChange} placeholder='fullName'/> 
       </div>
       <div className="formControl">
         <input type="email" name="email"  placeholder='email' value={toSend.email} onChange={handleChange} />
@@ -44,7 +55,7 @@ const onSubmit =(e) => {
       <div className="formControl">
         <input type="number" name="number" placeholder='number' value={toSend.number} onChange={handleChange}/>
       </div>
-      <textarea name="Message" id="" cols="60"  rows="10" placeholder='comment' className="textArea"  value={toSend.message} onChange={handleChange}></textarea>
+      <textarea name="message" id="" cols="60"  rows="10" placeholder='comment' className="textArea"  value={toSend.message} onChange={handleChange}></textarea>
       <input type="submit" name="name" className="button" />
     </form>
    
