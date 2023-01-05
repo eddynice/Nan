55c12c5337e2eced4495527d5a4732a30afc0ce9
Osaze <osareniyeosazee@gmail.com>
2023-01-05 23:06:39 +0100

    come

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     const [isNavExpanded, setIsNavExpanded] = useState(false)
     return (
       <nav className='navigation'>
-        <Link to="/" className='brand-name'>
+        <Link to="/" className='brand-name'    onClick={()=>{setIsNavExpanded(!isNavExpanded)}}>
           <DiCodeigniter/>SAZE
         </Link>
         <button className='hamburger' onClick={()=>{setIsNavExpanded(!isNavExpanded);
